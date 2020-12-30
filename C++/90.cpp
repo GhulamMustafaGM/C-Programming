46e15746182006d4f4c5cea9851ad41a62bf4e82
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 00:18:48 +0100

    C++ programming - Use the abs() function

---
+++ C++/90.cpp
@@ -0,0 +1,18 @@
+// Use the abs() function
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    cout << "\n"
+         << abs(-10);
+    cout << endl;
+    return 0;
+}
+
+/* output:
+
+	10
+*/
