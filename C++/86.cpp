b9c5d86df1566b954a7da1f5a7c38fc5e0c442e6
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 00:13:30 +0100

    C++ programming - using a variable

---
+++ C++/86.cpp
@@ -0,0 +1,19 @@
+// Using a variable
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;    // this declares a variable
+    x = 1023; // this assigns 1023 to x
+
+    cout << "\nThis program prints the values of x: ";
+    cout << x << endl; // This displays 1023
+
+    return 0;
+}
+
+/* Output
+This program prints the values of x: 1023
+*/
