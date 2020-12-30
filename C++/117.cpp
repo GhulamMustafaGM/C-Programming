17bf9908e7aa5b4b11951969840ee8a0e25807c5
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 00:52:34 +0100

    C++ programm - displays all printable characters

---
+++ C++/117.cpp
@@ -0,0 +1,24 @@
+// This program displays all printable characters, including the extended character set, if one exists.
+
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    unsigned char ch;
+
+    ch = 32;
+
+    while (ch)
+    {
+        cout << ch;
+        ch++;
+    }
+    return 0;
+}
+
+/* 
+
+ !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ÇüéâäàåçêëèïîìÄÅÉæÆôöòûùÿÖÜ¢£¥₧ƒáíóúñÑªº¿⌐¬½¼¡«»░▒▓│┤╡╢╖╕╣║╗╝╜╛┐└┴┬├─┼╞╟╚╔╩╦╠═╬╧╨╤╥╙╘╒╓╫╪┘┌█▄▌▐▀αßΓπΣσµτΦΘΩδ∞φε∩≡±≥≤⌠⌡÷≈°∙·√ⁿ²■ 
+*/
