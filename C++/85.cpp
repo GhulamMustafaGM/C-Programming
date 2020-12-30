9613d7eeb8f1c36e1d053dd57dfa263cbae00546
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 00:12:42 +0100

    C++ programming - simple

---
+++ C++/85.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+//  main() is where program execution begins.
+
+int main()
+{
+    cout << "This is my first C++ program.";
+
+    return 0;
+}
+
+/* Output
+
+This is my first C++ program.
+
+*/
