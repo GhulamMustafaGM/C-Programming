5bf11a62da9347ce91e08f39a7135a4743dcb00a
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-30 23:29:45 +0100

    C++ program - pattern program 5

---
+++ C++/57.cpp
@@ -0,0 +1,29 @@
+/* C++ pattern program 5*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, j, n = 1;
+
+    for (i = 0; i < 5; i +=)
+    {
+        for (j = 0; j <= i; j++)
+        {
+            cout << n << " ";
+            n++;
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
+/*output 
+
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+
+*/
