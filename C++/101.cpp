6a86197104dbed3bacf36316051938a936eb19c1
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 00:32:10 +0100

    C++ programm - literals values

---
+++ C++/101.cpp
@@ -0,0 +1,21 @@
+/* literals also called constants rerfer to fixed values.
+
+*/
+
+// character escape squences
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "\n\\b";
+
+    return 0;
+}
+
+/* output 
+
+\b
+
+*/
