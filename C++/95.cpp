49237091eee8c05abf562e24208f0b93179f9b69
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 00:24:31 +0100

    C++ programming - use the for loop

---
+++ C++/95.cpp
@@ -0,0 +1,22 @@
+// A program that illusrates the for loop.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int count;
+
+    cout << endl;
+    for (count = 1; count < 100; coun = count + 1)
+        cout << count << " ";
+    cout << endl;
+
+    return 0;
+}
+
+/* output:
+
+1 2 3 4 5 6 7 8 9 10
+
+*/
