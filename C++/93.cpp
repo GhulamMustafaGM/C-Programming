332e9ae067fa1462696d2365380397f0201cf9e6
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 00:22:30 +0100

    C++ programming - use a new line

---
+++ C++/93.cpp
@@ -0,0 +1,21 @@
+// This program demonstrates the \n code, which generates a new line.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "one\n";
+    cout << "two\n";
+    cout << "three\n";
+    cout << "four";
+
+    return 0;
+}
+
+/* Output:
+	one
+	two 
+	three
+	four
+*/
