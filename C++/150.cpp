eac0e2c6f2002d62340a5dee13ef931c5e86e27c
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 13:35:48 +0100

    C++ program - pointer

---
+++ C++/150.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char *s;
+
+    s = "Pointers are fun to use. \n";
+
+    cout << s;
+
+    return 0;
+}
+
+/* 
+
+Pointers are fun to use. 
+*/
