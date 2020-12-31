fdd7a74cade85a353b3f2513ec4721eb4d75cebf
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 21:03:05 +0100

    C++ Program - class

---
+++ 02-C++ Programing/61.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class base
+{
+    int i, j;
+
+public:
+    void set(int a, int b)
+    {
+        i = a;
+        j = b;
+    }
+    void show() { cout << i << " " << j << endl; }
+};
+
+class derived : public base
+{
+    int k;
+
+public:
+    derived(int x) { k = x; }
+    void showk() { cout << k << endl; }
+};
+
+int main()
+{
+    derived ob(3);
+
+    ob.set(1, 2); // access member of base
+    ob.show();    // access member of base
+
+    ob.showk(); // uses member of derived class
+
+    return 0;
+}
+
+/* output:
+1 2
+3
+
+*/
