18a126aa535b546f3c9dea29ae97c508191ec5d8
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 01:24:45 +0100

    C++ program - print a string backwards

---
+++ C++/136.cpp
@@ -0,0 +1,27 @@
+// Print a string backwards.
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    char str[80];
+    int i;
+
+    cout << "Enter a string: ";
+    gets(str);
+
+    // Print the string in reverse.
+
+    for (i = strlen(str) - 1; i >= 0; i--)
+        cout << str[i];
+    return 0;
+}
+
+/*
+Enter a string: Ghulam
+maluhG
+
+*/
