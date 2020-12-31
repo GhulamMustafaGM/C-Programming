860f09252131eea18ab923d18bb7e20371e7a793
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 13:45:45 +0100

    C++ program - calculation

---
+++ C++/159.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Enter a number: ";
+
+    int a; // declare one variable
+    cin >> a;
+
+    cout << "Enter a second number: ";
+    int b; // declare another variable
+    cin >> b;
+
+    cout << "Product: " << a * b << endl;
+    return 0;
+}
+
+/* Output: 
+
+Enter a number: 3
+Enter a second number: 2
+Product: 6
+
+*/
