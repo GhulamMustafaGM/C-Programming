29befa57d5227151dfe31905a5067aea39470f88
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 22:02:37 +0100

    C++ Program -

---
+++ 03-C++ Programing/15.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    char s[80];
+    cin >> ws >> s;
+
+    cout << s;
+
+    return 0;
+}
+
+/* output:
+
+No output
+
+*/
