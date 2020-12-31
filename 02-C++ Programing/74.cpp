035cb78b6715d862d454a7f3181daf9f2d963404
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 21:18:12 +0100

    C++ Program - explicit scopw resolution to select i

---
+++ 02-C++ Programing/74.cpp
@@ -0,0 +1,62 @@
+// This program uses explcit scope resolution to select i.
+
+#include <iostream>
+using namespace std;
+class base
+{
+public:
+    int i;
+};
+
+// derived1 inherits base.
+class derived1 : public base
+{
+public:
+    int j;
+};
+
+// derived2 inherits base.
+
+class derived2 : public base
+{
+public:
+    int k;
+};
+
+/* derived3 inherits both derived1 and derived2.
+	This means that there are two compies of base
+	in derived3!
+*/
+
+class derived3 : public derived1, public derived2
+{
+public:
+    int sum;
+};
+
+int main()
+{
+    derived3 ob;
+
+    ob.derived1::i = 10; // scope resolved, use derived1's i
+    ob.j = 20;
+    ob.k = 30;
+
+    // scope resolved
+
+    ob.sum = ob.derived1::i + ob.j + ob.k;
+
+    // also resolved here
+    cout << ob.derived1::i << " ";
+
+    cout << ob.j << " " << ob.k << " ";
+    cout << ob.sum;
+
+    return 0;
+}
+
+/* output:
+
+10 20 30 60
+
+*/
