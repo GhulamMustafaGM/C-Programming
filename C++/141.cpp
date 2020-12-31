b7f27d4d4ff24e353a4fb5584d40e50229755fd7
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 01:31:31 +0100

    C++ program

---
+++ C++/141.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void f1();
+
+int main()
+{
+    f1();
+    f1();
+
+    return 0;
+}
+void f1()
+{
+    char s[80] = "this is a test\n";
+
+    cout << s;
+
+    strcpy(s, "CHANGED\n"); // change s
+    cout << s;
+}
+
+/* output:
+
+this is a test
+CHANGED
+this is a test
+CHANGED
+
+*/
