63bd3db54247f5ad4d3aedfe8b298a289fb45bc1
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 21:17:14 +0100

    C++ Program - an error and will not compile

---
+++ 02-C++ Programing/73.cpp
@@ -0,0 +1,64 @@
+// This program contains an error and will not compile.
+
+#include <iostream>
+using namespace std;
+
+class base
+{
+public:
+    int i;
+};
+
+// derived1 inherits base.
+
+class derived1 : public base
+{
+public:
+    int j;
+};
+
+// derived2 inherits base.
+
+class derived2 : public base
+{
+public:
+    int k;
+};
+
+/* derived3 inherits both derived1 and derived2.
+	This means that there are two compies of base
+	in dreived3! 
+*/
+
+class derived3 : public derived1, public derived2
+{
+public:
+    int sum;
+};
+
+int main()
+{
+    derived 3 ob;
+
+    ob.i = 10; // tis is ambiguous; which i????
+
+    ob.j = 20;
+
+    b.k = 30;
+
+    // i ambiguous here, too
+    ob.sum = ob.i + ob.j + ob.k;
+
+    // also ambiguous, which i?
+
+    cout << ob.i << " ";
+
+    cout << ob.j << " " << ob.k << " ";
+    cout << ob.sum;
+
+    return 0;
+}
+
+/* 
+No output. 
+*/
