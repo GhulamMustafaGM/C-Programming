7a9173a46ac47da76711f8644a02284c10de5c54
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 20:16:21 +0100

    C++ Program - use a class

---
+++ 02-C++ Programing/20.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+using namespace std;
+
+class myclass
+{
+    int a;
+
+public:
+    myclass(int x);
+    int get_a();
+};
+myclass::myclass(int x)
+{
+    a = x;
+}
+int myclass::get_a()
+{
+
+    return a;
+}
+int main()
+{
+    myclass ob = 4; // calls myclass(4)
+    cout << ob.get_a();
+
+    return 0;
+}
+
+/* output:
+
+4
+
+*/
