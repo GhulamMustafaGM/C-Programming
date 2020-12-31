0b039107f20f617f8ad2b6d35e3c22dd906a2331
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 23:25:25 +0100

    C++ Program

---
+++ 03-C++ Programing/78.cpp
@@ -0,0 +1,18 @@
+// This program is now fixed.
+#include <iostream>
+using namespace std;
+#define EVEN(a) (a) % 2 == 0 ? 1 : 0
+int main()
+{
+    if (EVEN(9 + 1))
+        cout << "is even";
+    else
+        cout << "is odd";
+    return 0;
+}
+
+/* output 
+
+is even
+
+*/
