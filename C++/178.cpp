046b92e0a8979449c9dcff641e72d42d8ac69870
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 14:02:28 +0100

    C++ program - fucntion

---
+++ C++/178.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int sqr_it(int x);
+
+int main()
+{
+    int t = 10;
+
+    cout << sqr_it(t) << ' ' << t;
+
+    return 0;
+}
+int sqr_it(int x)
+{
+    x = x * x;
+    return x;
+}
+
+/* output:
+
+100 10
+
+*/
