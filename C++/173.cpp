1b21fef497c4ebe53d4911556719b6d5fcc1d824
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 13:58:19 +0100

    C++ program

---
+++ C++/173.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    int i;
+    i = abs(-10);     // line 1
+    cout << abs(-23); // line 2
+    abs(100);         // line 3
+
+    return 0;
+}
+
+/* Output:
+
+23
+
+*/
