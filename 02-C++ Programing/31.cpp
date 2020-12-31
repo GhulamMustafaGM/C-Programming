5e2169384a1d09bdb1d83c77233063f47d3265e6
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 20:26:21 +0100

    C++ Program - using friend function

---
+++ 02-C++ Programing/31.cpp
@@ -0,0 +1,43 @@
+// Demonstrate a friend funciton.
+
+#include <iostream>
+using namespace std;
+
+class myclass
+{
+    int a, b;
+
+public:
+    myclass(int i, int j)
+    {
+        a = i;
+        b = j;
+    }
+
+    friend int sum(myclass x); // sum() is a friend of myclass
+};
+
+// Note: sum() is not a member function of any class.
+
+int sum(myclass x)
+{
+    /* Becuase sum() is a friend of myclass, it can 
+		directly access a and b. */
+
+    return x.a + x.b;
+}
+
+int main()
+{
+    myclass n(3, 4);
+
+    cout << sum(n);
+
+    return 0;
+}
+
+/* output: 
+
+7
+
+*/
