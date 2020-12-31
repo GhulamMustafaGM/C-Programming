66f8b925d94689c1229369397c5e412c3877ca2f
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 22:19:00 +0100

    C++ Program - using typeid with templates

---
+++ 03-C++ Programing/31.cpp
@@ -0,0 +1,50 @@
+// Using typeid with templates.
+#include <iostream>
+// #include<typeinfo>
+using namespace std;
+
+template <class T>
+class myclass
+{
+    T a;
+
+public:
+    myclass(T i) { a = i; }
+    // ...
+};
+int main()
+{
+    myclass<int> o1(10), o2(9);
+    myclass<double> o3(7.2);
+
+    cout << "Type of o1 is ";
+    cout << typeid(o1).name() << endl;
+
+    cout << "Type of o2 is ";
+    cout << typeid(o2).name() << endl;
+
+    cout << "Type of o3 is ";
+
+    cout << typeid(o3).name() << endl;
+    cout << endl;
+
+    if (typeid(o1) == typeid(o2))
+        cout << "o1 and o2 are the same type\n";
+    if (typeid(o1) == typeid(o3))
+        cout << "Error\n";
+    else
+        cout << "o1 and o3 are different types\n";
+
+    return 0;
+}
+
+/* Output:
+
+Type of o1 is class myclass<int>
+Type of o2 is class myclass<int>
+Type of o3 is class myclass<double>
+
+o1 and o2 are the same type
+o1 and o3 are different types
+
+*/
