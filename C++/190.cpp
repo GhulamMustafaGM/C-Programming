7a93fe15e1eccc129344d6220cec8f7b38999cb1
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 14:11:58 +0100

    C++ Program - print overloading ambigity

---
+++ C++/190.cpp
@@ -0,0 +1,30 @@
+// Overloading ambiguity.
+
+#include <iostream>
+using namespace std;
+
+float myfunc(float i);
+double myfunc(double i);
+int main()
+{
+    // unambiguous, calls myfunc(double)
+    cout << myfunc(10.1) << " ";
+    // ambiguous
+    cout << myfunc(10);
+
+    return 0;
+}
+float myfunc(float i)
+{
+    return i;
+}
+double myfunc(double i)
+{
+    return -i;
+}
+
+/* 
+
+*** Error ***
+
+*/
