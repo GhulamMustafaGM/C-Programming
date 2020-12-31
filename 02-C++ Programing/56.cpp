45ccad393e6a27fc97427b406ba9a2931f870931
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 20:59:12 +0100

    C++ Program - safe array

---
+++ 02-C++ Programing/56.cpp
@@ -0,0 +1,59 @@
+// A safe array example.
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+const int SIZE = 3;
+
+class atype
+{
+    int a[SIZE];
+
+public:
+    atype()
+    {
+        register int i;
+
+        for (i = 0; i < SIZE; i++)
+            a[i] = i;
+    }
+    int &operator[](int i);
+};
+
+// Provide range checking for atype.
+
+int &atype::operator[](int i)
+{
+    if (i < 0 || i > SIZE - 1)
+    {
+        cout << "\nIndex value of ";
+        cout << i << " is out-of-bounds.\n";
+        exit(1);
+    }
+
+    return a[i];
+}
+
+int main()
+{
+    atype ob;
+
+    cout << ob[2]; // displays 2
+    cout << " ";
+
+    ob[2] = 25; // [] appears on left
+
+    cout << ob[2]; // displays 25
+
+    ob[3] = 44; // generates runtime error, 3 out-of-range
+
+    return 0;
+}
+
+/* output:
+
+2 25
+Index value of 3 is out-of-bounds.
+
+*/
