22f4019f2fd8721938ea6898aa894991918846f4
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 21:09:52 +0100

    C++ Program - constructors and destructors

---
+++ 02-C++ Programing/66.cpp
@@ -0,0 +1,36 @@
+// Ehen construcytors and destructors are executed
+
+#include <iostream>
+using namespace std;
+
+class base
+{
+    base() { cout << "Constructing base\n"; }
+    ~base() { "Destructing base\n"; }
+};
+
+class derived : public base
+{
+public:
+    derived() { cout << "Constructing derived\n"; }
+    ~derived() { cout << "Destructing derived\n"; }
+};
+
+int main()
+
+{
+    derived ob;
+
+    // do nothing but construct and destruct ob
+
+    return 0;
+}
+
+/* output:
+
+Constructing base
+Constructing derived
+Destructing derived
+Destructing base
+
+*/
