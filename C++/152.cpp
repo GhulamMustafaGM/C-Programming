39653300a64b414daf1f564d3829a8b73b3d608b
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 13:38:28 +0100

    C++ program - pointers

---
+++ C++/152.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <conio.h>
+using namespace std;
+
+char *fortunes[]{
+    "Soon, you will come into some money. \n",
+    "A new love will enter your life.\n",
+    "You will live long and prosper. \n",
+    "Now is a good time to invest for the future.\n",
+    "A close friend will as for a favor. \n"};
+
+int main()
+{
+    int chance;
+
+    cout << "To see eyour fortune, press a key: ";
+
+    // randomize the random number generator
+
+    while (!kbhit())
+        rand();
+
+    cout << endl;
+
+    chance = rand();
+    chance = chance % 5;
+    cout << fortuness[chance];
+
+    return 0;
+}
