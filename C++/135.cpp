b8be3880e73586446153f4e0227ff05c05304e20
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 01:23:51 +0100

    C++ program - using strlen(s)

---
+++ C++/135.cpp
@@ -0,0 +1,26 @@
+// strlen(s);
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+int main()
+
+{
+    char str[80];
+
+    cout << "Enter a string: ";
+    gets(s);
+
+    cout << "Length is: ";
+    cout << strlen(s);
+    cout << endl;
+    return 0;
+}
+
+/*
+
+Enter a string: hello there
+Length is: 11
+*/
