66f9c7e401800d372ef979846f33a8e261da4a76
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 14:25:09 +0100

    C++ Program - demonstrate data types and parameters

---
+++ C++/205.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, j;
+
+    j = 10;
+
+    i = (j++, j + 100, 999 + j);
+
+    cout << i;
+
+    return 0;
+}
+
+/* output:
+
+1010
+
+*/
