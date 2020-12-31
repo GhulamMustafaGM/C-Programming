d527c6df59a2efb94d76e823323a51664515c0db
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-31 22:01:22 +0100

    C++ Program - three functions width(), fill(), precision()

---
+++ 03-C++ Programing/13.cpp
@@ -0,0 +1,35 @@
+// Demonstrates three functions width(), fill(), precision()
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout.setf(ios::showpos);
+    cout.setf(ios::scientific);
+    cout << 123 << " " << 123.23 << "\n";
+
+    cout.precision(2); // two digits after decimal point
+    cout.width(10);    // in a field of 10 characters
+    cout << 123 << " ";
+    cout.width(10); // set width to 10
+    cout << 123.23 << "\n";
+
+    cout.fill('#'); // fill using #
+    cout.width(10); // in a field of 10 characters
+    cout << 123 << " ";
+    cout.width(10); // set width to 10
+    cout << 123.23;
+    cout << endl;
+
+    return 0;
+}
+
+/* Output:
+
++123 +1.232300e+002
+      +123 +1.23e+002
+######+123 +1.23e+002
+
+*/
