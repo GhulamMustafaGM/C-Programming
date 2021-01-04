5e87f64275cb1f78ee0f95f6e52977329fdc0205
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-04 19:23:20 +0100

    C++ Programming - a + b^2 program

---
+++ 04-C++ Programming/26.cpp
@@ -0,0 +1,45 @@
+// a +b^2 program
+
+#include <iostream>
+
+using namespace std;
+
+class Mathematics
+{
+    int a, b;
+
+public:
+    void input()
+    {
+        cout << "Input a:\n";
+        cin >> a;
+        cout << "Input b:\n\n";
+        cin >> b;
+    }
+
+    void add()
+    {
+        cout << "(a+b)^2 = " << ((a * a) + (2 * a * b) + (b * b));
+    }
+};
+
+int main()
+{
+    Mathematics m; // Creating object of class
+
+    m.input();
+    m.add();
+
+    return 0;
+}
+
+/* Output:
+
+Input a:
+2
+Input b:
+
+3
+(a+b)^2 = 25
+
+ */
