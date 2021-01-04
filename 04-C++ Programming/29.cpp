05d0798cdefef9de94ed2decdb3accd1b193898f
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-04 19:28:22 +0100

    C++ Programming - ASCII value of number

---
+++ 04-C++ Programming/29.cpp
@@ -0,0 +1,27 @@
+// ASCII value of numbrer
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //variable declaration
+    char asciichar;
+
+    //take user input
+    cout << "Enter any character : ";
+    cin >> asciichar;
+
+    //display ascii value of character
+    cout << "\nIt's ascii  value is here: " << (int)asciichar << endl;
+
+    return 0;
+}
+
+/*
+Enter any character : u
+
+It's ascii  value is here: 117
+
+*/
