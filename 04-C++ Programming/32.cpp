20e2761bea259cf917f0330a116fece0460c5af0
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-04 19:35:01 +0100

    C++ Programming - Areas of circle

---
+++ 04-C++ Programming/32.cpp
@@ -0,0 +1,27 @@
+// Areas of circle
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    float r, a;
+
+    cout << "Enter Radius : ";
+    cin >> r;
+
+    a = 3.14 * r * r;
+
+    cout << "Area of Circle : " << a;
+    cout << endl;
+
+    return 0;
+}
+
+/* Output:
+
+Enter Radius : 45
+Area of Circle : 6358.5
+
+*/
