0baca0588c9cd1df67350da1390cc13ebbf4d1c0
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-06 16:35:06 +0100

    C++ Programming - fnd  a+b^2

---
+++ 04-C++ Programming/79.cpp
@@ -0,0 +1,34 @@
+// Find a+b^2
+
+#include <iostream>
+
+using namespace std;
+
+class Mathematics
+{
+    int a, b;
+
+public:
+    void input()
+    {
+        cout << "Input a:\n";
+        cin >> a;
+        cout << "Input b:\n\n";
+        cin >> b;
+    }
+
+    void add()
+    {
+        cout << "(a+b)^2 = " << ((a * a) + (2 * a * b) + (b * b));
+    }
+};
+
+int main()
+{
+    Mathematics m; // Creating object of class
+
+    m.input();
+    m.add();
+
+    return 0;
+}
