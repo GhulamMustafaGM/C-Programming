d9b40d2558ae55773b3008cd403bd18dc71a744a
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-06 16:18:46 +0100

    C++ Programming -

---
+++ 04-C++ Programming/70.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    char eng[52] = {'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z'};
+    int i;
+    cin >> i;
+    if (i <= 51)
+    {
+        cout << eng[i] << endl;
+    }
+    return 0;
+}
