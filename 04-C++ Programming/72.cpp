fed74c98517056989f6bc972f79c0d96775e2eda
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-06 16:22:49 +0100

    C++ Programming - ASSCII value of numbers

---
+++ 04-C++ Programming/72.cpp
@@ -0,0 +1,20 @@
+// ASSCII value of number
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Variable declaration
+    char asciichar;
+
+    //Take user input
+    cout << "Enter any character : ";
+    cin >> asciichar;
+
+    //Display ascii value of character
+    cout << "\nIt's ASCII value is here: " << (int)asciichar << endl;
+
+    return 0;
+}
