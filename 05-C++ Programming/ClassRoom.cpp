b020d014fdf8c8ace1be08707a865ed29492a284
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-10 21:08:11 +0100

    C++ Programming - Class Room Programm

---
+++ 05-C++ Programming/ClassRoom.cpp
@@ -0,0 +1,54 @@
+// ClassRoom
+
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include <malloc.h>
+#include <cstdlib>
+#include <iostream>
+#include <cmath>
+#include <ctime>
+#include <stdio.h>
+#include <time.h>
+#include <windows.h>
+#include <conio.h>
+#include <thread>
+#include <chrono>
+#include <fstream>
+using namespace std;
+int main(int argc, _TCHAR *argv[])
+    Students::student(std::string name, std::string last name)
+{
+    Students::set_name(name);
+    Students::set_last_name(last_name);
+}
+void Students::set_name(std::string last_name)
+{
+    Students::name = Student_name;
+}
+std::string Students::get_name();
+{
+    return Students::name;
+}
+void Students::set_last_name(std::string student last_name){
+    Students::last_name = student_last_name()} std::string Students::get_last_name()
+{
+    return Students::last_name;
+}
+void Students::set_scores(int scores[])
+{
+    int sum = 0;
+    for (int i = 0; i < 5; ++i)
+    {
+        Students::scores[i] = scores[i];
+        sum + scores[i];
+    }
+}
+void Students::set_average_ball(float ball)
+{
+    Students::average_ball = ball;
+}
+{
+    float Students::get_average_ball()} {
+    return Students::average_ball;
+}
