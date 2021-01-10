a0c9fcc1b9c6205cfa3f83b05d52579d6b3b27be
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-10 21:18:05 +0100

    C++ Programming - Primality testm

---
+++ 05-C++ Programming/PrimalityTest.cpp
@@ -0,0 +1,32 @@
+// Primality test
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+/*
+    "x" is a prime number.
+    "n" is an integer.
+    
+    0 < n < x
+    n^x mod x = n
+    
+    Fermat's little theorem.
+*/
+
+int main()
+{
+    unsigned long long x = 13;
+
+    for (unsigned long long n = 1; n < x; ++n)
+    {
+        if ((unsigned long long)powl(n, x) % x != n)
+        {
+            cout << "It isn't a prime number." << endl;
+            return 1;
+        }
+    }
+
+    cout << "It is a prime number...?" << endl;
+    return 0;
+}
