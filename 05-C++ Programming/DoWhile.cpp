e665cfe79ed27c077dc02c7af92194258402f176
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-10 23:41:04 +0100

    C++ Programming - do while loop

---
+++ 05-C++ Programming/DoWhile.cpp
@@ -0,0 +1,15 @@
+// do while loop
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 42;
+    do
+    {
+        cout << a << endl;
+    } while (a > 42);
+
+    return 0;
+}
