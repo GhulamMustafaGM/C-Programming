fe10a3d46549e08089bd0b1d56542089673ccef9
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-13 19:19:00 +0100

    C++ Programming - Find even, odd, and prime number

---
+++ 06-C++ Programming/FindEvenOdd.cpp
@@ -0,0 +1,35 @@
+// Find odd, even and prime number
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+    if (num % 2 == 0)
+    {
+        cout << num << " : is an even number" << endl;
+    }
+    if (num % 2 == 1)
+    {
+        cout << num << " : is an odd number" << endl;
+    }
+    int prime = 0;
+    for (int n = 1; n <= num; n++)
+    {
+        if (num % n == 0)
+        {
+            prime++;
+        };
+    };
+    if (prime == 2)
+    {
+        cout << num << " : is a prime number" << endl;
+    }
+    else
+    {
+        cout << num << " : is not a prime number" << endl;
+    }
+    return 0;
+}
