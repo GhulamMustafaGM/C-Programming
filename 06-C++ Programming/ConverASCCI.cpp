f3587f41afcfa45cc60170e1f247f2f5a28438be
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-15 23:43:26 +0100

    C++ Programming - Convert ASCCI to Char

---
+++ 06-C++ Programming/ConverASCCI.cpp
@@ -0,0 +1,17 @@
+// Convert ascii to char
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    char c;
+    
+    // Enter that ascii value that you want to know character
+    cin >> x;
+    c = (char)x;
+    cout << "The value of this ascii value is :" << c;
+
+    return 0;
+}
