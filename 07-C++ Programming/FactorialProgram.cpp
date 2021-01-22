314cf7fd4d410ae2541e5f2c1bbc5c32407ea4bc
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-22 21:56:45 +0100

    C++ Programming - Factorial Program

---
+++ 07-C++ Programming/FactorialProgram.cpp
@@ -0,0 +1,20 @@
+// Factoral
+
+> #include<iostream> using namespace std;
+
+int factorial(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * factorial(n - 1);
+    }
+}
+
+int main()
+{
+    cout << factorial(5);
+}
