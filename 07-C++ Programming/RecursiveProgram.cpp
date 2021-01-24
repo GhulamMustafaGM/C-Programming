0167e945360efb8178fddc3c74164f37f24b2e77
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-24 21:58:03 +0100

    C++ Program - Recursive Program

--- 07-C++ Programming/RecursiveProgram.cpp
+++ 07-C++ Programming/RecursiveProgram.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -20,4 +21,43 @@ int main()
         cout << elem << "=100\n";
 
     return 0;
+}
+
+//recursive generate '+' '-' 'space' between numbers + inspection
+void generate(list<string> &l, string &numb, int pos, int sum)
+{
+    static const string element = "+ -";
+
+    if (pos >= numb.length())
+    {
+        //inspection
+        string s = numb;
+        if (insp(s, sum))
+            l.push_back(s);
+        return;
+    }
+
+    int i = (pos) ? 0 : 1;
+    for (; i < element.size(); i++)
+    {
+        numb[pos] = element[i];
+        generate(l, numb, pos + 2);
+    }
+}
+
+//inspection
+bool insp(string &s, int res)
+{
+    //erase space
+    while (s.find(" ") != -1)
+        s.erase(s.find(" "), 1);
+
+    //calculate summa
+    int sum = 0, num;
+    stringstream buffer(s);
+    while (buffer >> num)
+        sum += num;
+
+    //inspection
+    return (sum == res);
 }
