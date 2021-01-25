c3f7099e2d95e7b213ebd7d3bdc736b22973aa0c
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-25 19:48:50 +0100

    C++ Program - Cube Program

---
+++ 07-C++ Programming/CubeProgram.cpp
@@ -0,0 +1,22 @@
+// Cube c++
+
+#include <iostream>
+using namespace std;
+string space = "     ";
+int main()
+{
+    cout << "         ______________" << endl;
+    cout << "        /|            /|" << endl;
+    cout << "       / |           / |" << endl;
+    cout << "      /  |          /  |" << endl;
+    cout << "     /___|_________/   |" << endl;
+    cout << "    |    |         |   |" << endl;
+    cout << "    |    |         |   |" << endl;
+    cout << "    |    |_________|___|" << endl;
+    cout << "    |   /          |   /" << endl;
+    cout << "    |  /           |  / " << endl;
+    cout << "    | /            | /  " << endl;
+    cout << "    |/_____________|/ " << endl;
+
+    return 0;
+}
