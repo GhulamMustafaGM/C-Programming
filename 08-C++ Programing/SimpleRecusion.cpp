da1790d297cf14c75eda763ecb54c662ac557757
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-29 23:23:16 +0100

    C++ Programming - Simple recusrion

---
+++ 08-C++ Programing/SimpleRecusion.cpp
@@ -0,0 +1,19 @@
+// Imple recursion
+
+#include <iostream>
+using namespace std;
+int fun(int x)
+{
+    if (x == 0)
+        return 0;
+    else if ((x % 10) % 2 == 0)
+        return (1 + fun(x / 10));
+    return (fun(x / 10));
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout << fun(n);
+    return 0;
+}
