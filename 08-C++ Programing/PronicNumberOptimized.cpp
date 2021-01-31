f52805b304962e2dae5d702218f10b47981fe39a
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-31 23:15:35 +0100

    C++ Programming - Pronic number optimized

---
+++ 08-C++ Programing/PronicNumberOptimized.cpp
@@ -0,0 +1,33 @@
+// pronic number optimised
+
+#include <iostream>
+
+using namespace std;
+void amount(float princ, int t = 2, float rate = .08)
+{
+
+    cout << " principal amount :" << princ;
+    cout << endl;
+    cout << "time :" << t << " years";
+    cout << endl;
+    cout << "rate:" << rate;
+    cout << endl;
+    cout << "interest amount: " << (princ * t * rate);
+    cout << endl;
+}
+int main()
+{
+
+    cout << " case 1";
+    cout << endl;
+    amount(2000);
+    cout << endl;
+    cout << "case 2 ";
+    amount(2300, 3);
+    cout << "case 3";
+    amount(2300, 3, 0.11);
+    cout << "case 4";
+    amount(2500, 0.12);
+
+    return 0;
+}
