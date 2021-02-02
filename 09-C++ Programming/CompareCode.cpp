0049916ee191523cdaa4d9fd683896dd46d3f5b7
Ghulam Mustafa <mustafaji@gmail.com>
2021-02-02 23:21:24 +0100

    C++ Programming - Compare code

---
+++ 09-C++ Programming/CompareCode.cpp
@@ -0,0 +1,21 @@
+// Compare code
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << " put valve for a \n ";
+    cin >> a;
+    cout << " put valve for b \n ";
+    cin >> b;
+    if (a > b)
+    {
+        cout << "a is greater than b";
+    }
+    if (a < b)
+    {
+        cout << "b is grater than a";
+    }
+    return 0;
+}
