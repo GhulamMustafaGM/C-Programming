b9233b7ac91699ab72cea3b6b7364a5d077782a0
Ghulam Mustafa <mustafaji@gmail.com>
2021-02-03 22:52:15 +0100

    C++ Programming - Magic with text

---
+++ 09-C++ Programming/MagicText.cpp
@@ -0,0 +1,336 @@
+// Magic with Text
+
+#include <iostream>
+#include <conio.h>
+#include <cstdlib>
+
+using namespace std;
+string a()
+{
+    cout << "    /\\" << endl;
+    cout << "   /  \\" << endl;
+    cout << "  / /\\ \\" << endl;
+    cout << " / ____ \\" << endl;
+    cout << "/_/    \\_\\" << endl;
+    return "";
+}
+string b()
+{
+    cout << " ____" << endl;
+    cout << "|  _ \\" << endl;
+    cout << "| |_) |" << endl;
+    cout << "|  _ <" << endl;
+    cout << "| |_) |" << endl;
+    cout << "|____/" << endl;
+    return "";
+}
+string c()
+{
+    cout << " _____" << endl;
+    cout << "/ ____|" << endl;
+    cout << "| |" << endl;
+    cout << "| |    " << endl;
+    cout << "| |____" << endl;
+    cout << "\\_____|" << endl;
+    return "";
+}
+string d()
+{
+    cout << " _____  " << endl;
+    cout << "|  __ \\ " << endl;
+    cout << "| |  | |" << endl;
+    cout << "| |  | |" << endl;
+    cout << "| |__| |" << endl;
+    cout << "|_____/ " << endl;
+    return "";
+}
+string e()
+{
+    cout << " ______ " << endl;
+    cout << "|  ____|" << endl;
+    cout << "| |__   " << endl;
+    cout << "|  __|  " << endl;
+    cout << "| |____ " << endl;
+    cout << "|______|" << endl;
+    return "";
+}
+string f()
+{
+    cout << " ______ " << endl;
+    cout << "|  ____|" << endl;
+    cout << "| |__   " << endl;
+    cout << "|  __|  " << endl;
+    cout << "| |     " << endl;
+    cout << "|_|     " << endl;
+    return "";
+}
+string g()
+{
+    cout << " _____  " << endl;
+    cout << "/ ____| " << endl;
+    cout << "| |  __ " << endl;
+    cout << "| | |_ |" << endl;
+    cout << "| |__| |" << endl;
+    cout << "\\_____| " << endl;
+    return "";
+}
+string h()
+{
+    cout << " _    _ " << endl;
+    cout << "| |  | |" << endl;
+    cout << "| |__| |" << endl;
+    cout << "|  __  |" << endl;
+    cout << "| |  | |" << endl;
+    cout << "|_|  |_|" << endl;
+    return "";
+}
+string ii()
+{
+    cout << " _____ " << endl;
+    cout << "|_   _|" << endl;
+    cout << "  | |  " << endl;
+    cout << "  | |  " << endl;
+    cout << " _| |_ " << endl;
+    cout << "|_____|" << endl;
+
+    return "";
+}
+string j()
+{
+    cout << "      _ " << endl;
+    cout << "     | |" << endl;
+    cout << "     | |" << endl;
+    cout << " _   | |" << endl;
+    cout << "| |__| |" << endl;
+    cout << " \\____/ " << endl;
+    return "";
+}
+string k()
+{
+    cout << " _  __ " << endl;
+    cout << "| |/ / " << endl;
+    cout << "| ' /  " << endl;
+    cout << "|  <   " << endl;
+    cout << "| . \\  " << endl;
+    cout << "|_|\\_\\ " << endl;
+    return "";
+}
+string l()
+{
+    cout << " _      " << endl;
+    cout << "| |     " << endl;
+    cout << "| |     " << endl;
+    cout << "| |     " << endl;
+    cout << "| |____ " << endl;
+    cout << "|______|" << endl;
+    return "";
+}
+string m()
+{
+    cout << " __  __ " << endl;
+    cout << "|  \\/  |" << endl;
+    cout << "| \\  / |" << endl;
+    cout << "| |\\/| |" << endl;
+    cout << "| |  | |" << endl;
+    cout << "|_|  |_|" << endl;
+    return "";
+}
+string n()
+{
+    cout << " _   _ " << endl;
+    cout << "| \\ | |" << endl;
+    cout << "|  \\| |" << endl;
+    cout << "| . ` |" << endl;
+    cout << "| |\\  |" << endl;
+    cout << "|_| \\_|" << endl;
+    return "";
+}
+string o()
+{
+    cout << "  ____  " << endl;
+    cout << " / __ \\ " << endl;
+    cout << "| |  | |" << endl;
+    cout << "| |  | |" << endl;
+    cout << "| |__| |" << endl;
+    cout << " \\____/ " << endl;
+    return "";
+}
+string p()
+{
+    cout << " _____  " << endl;
+    cout << "|  __ \\ " << endl;
+    cout << "| |__) |" << endl;
+    cout << "|  ___/ " << endl;
+    cout << "| |     " << endl;
+    cout << "|_|     " << endl;
+    return "";
+}
+string q() //
+{
+    cout << "  ____" << endl;
+    cout << " / __ \\ " << endl;
+    cout << "| |  | | " << endl;
+    cout << "| |  | | " << endl;
+    cout << "| |__| | " << endl;
+    cout << " \\___\\_\\ " << endl;
+    return "";
+}
+string r()
+{
+    cout << " _____" << endl;
+    cout << "|  __ \\" << endl;
+    cout << "| |__) |" << endl;
+    cout << "|  _  /" << endl;
+    cout << "| | \\ \\" << endl;
+    cout << "|_|  \\_\\" << endl;
+    return "";
+}
+string s()
+{
+    cout << "   _____" << endl;
+    cout << " / ____|" << endl;
+    cout << "| (___  " << endl;
+    cout << " \\___ \\ " << endl;
+    cout << " ____) | " << endl;
+    cout << "|_____/ " << endl;
+    return "";
+}
+string t() //
+{
+    cout << " _______" << endl;
+    cout << "|__   __|" << endl;
+    cout << "   | |" << endl;
+    cout << "   | |" << endl;
+    cout << "   | |" << endl;
+    cout << "   |_|" << endl;
+
+    return "";
+}
+string u()
+{
+    cout << " _    _ " << endl;
+    cout << "| |  | |" << endl;
+    cout << "| |  | |" << endl;
+    cout << "| |  | |" << endl;
+    cout << "| |__| |" << endl;
+    cout << " \\____/" << endl;
+    return "";
+}
+string v()
+{
+    cout << "__      __" << endl;
+    cout << "\\ \\    / /" << endl;
+    cout << " \\ \\  / /" << endl;
+    cout << "  \\ \\/ /" << endl;
+    cout << "   \\  /" << endl;
+    cout << "    \\/ " << endl;
+    return "";
+}
+string w()
+{
+    cout << "__          __" << endl;
+    cout << "\\ \\        / /" << endl;
+    cout << " \\ \\  /\\  / /" << endl;
+    cout << "  \\ \\/  \\/ /" << endl;
+    cout << "   \\  /\\  /" << endl;
+    cout << "    \\/  \\/" << endl;
+    return "";
+}
+string x() //
+{
+    cout << " __   __" << endl;
+    cout << " \\ \\ / /" << endl;
+    cout << "  \\ V /" << endl;
+    cout << "   > <" << endl;
+    cout << "  / . \\" << endl;
+    cout << " /_/ \\_\\" << endl;
+    return "";
+}
+string y()
+{
+    cout << "__     __" << endl;
+    cout << "\\ \\   / /" << endl;
+    cout << " \\ \\_/ /" << endl;
+    cout << "  \\   /" << endl;
+    cout << "   | |" << endl;
+    cout << "   |_|" << endl;
+    return "";
+}
+string z()
+{
+    cout << " ______" << endl;
+    cout << "|___  /" << endl;
+    cout << "   / /" << endl;
+    cout << "  / /" << endl;
+    cout << " / /__" << endl;
+    cout << "/_____|" << endl;
+    return "";
+}
+
+string wybor;
+
+int main()
+{
+    string wyraz;
+    std::getline(std::cin, wyraz);
+    int dlugosc = wyraz.length();
+    system("cls");
+
+    for (int i = dlugosc - dlugosc; i <= dlugosc; i++)
+    {
+
+        if ((wyraz[i] == 'a') || (wyraz[i] == 'A'))
+            cout << a();
+        if ((wyraz[i] == 'b') || (wyraz[i] == 'B'))
+            cout << b();
+        if ((wyraz[i] == 'c') || (wyraz[i] == 'C'))
+            cout << c();
+        if ((wyraz[i] == 'd') || (wyraz[i] == 'D'))
+            cout << d();
+        if ((wyraz[i] == 'e') || (wyraz[i] == 'E'))
+            cout << e();
+        if ((wyraz[i] == 'f') || (wyraz[i] == 'F'))
+            cout << f();
+        if ((wyraz[i] == 'g') || (wyraz[i] == 'G'))
+            cout << g();
+        if ((wyraz[i] == 'h') || (wyraz[i] == 'H'))
+            cout << h();
+        if ((wyraz[i] == 'i') || (wyraz[i] == 'I'))
+            cout << ii();
+        if ((wyraz[i] == 'j') || (wyraz[i] == 'J'))
+            cout << j();
+        if ((wyraz[i] == 'k') || (wyraz[i] == 'K'))
+            cout << k();
+        if ((wyraz[i] == 'l') || (wyraz[i] == 'L'))
+            cout << l();
+        if ((wyraz[i] == 'm') || (wyraz[i] == 'M'))
+            cout << m();
+        if ((wyraz[i] == 'n') || (wyraz[i] == 'N'))
+            cout << n();
+        if ((wyraz[i] == 'o') || (wyraz[i] == 'O'))
+            cout << o();
+        if ((wyraz[i] == 'p') || (wyraz[i] == 'P'))
+            cout << p();
+        if ((wyraz[i] == 'q') || (wyraz[i] == 'Q'))
+            cout << q();
+        if ((wyraz[i] == 'r') || (wyraz[i] == 'R'))
+            cout << r();
+        if ((wyraz[i] == 's') || (wyraz[i] == 'S'))
+            cout << s();
+        if ((wyraz[i] == 't') || (wyraz[i] == 'T'))
+            cout << t();
+        if ((wyraz[i] == 'u') || (wyraz[i] == 'U'))
+            cout << u();
+        if ((wyraz[i] == 'v') || (wyraz[i] == 'V'))
+            cout << v();
+        if ((wyraz[i] == 'w') || (wyraz[i] == 'W'))
+            cout << w();
+        if ((wyraz[i] == 'x') || (wyraz[i] == 'X'))
+            cout << x();
+        if ((wyraz[i] == 'y') || (wyraz[i] == 'Y'))
+            cout << y();
+        if ((wyraz[i] == 'z') || (wyraz[i] == 'Z'))
+            cout << z();
+    }
+    return 0;
+}
