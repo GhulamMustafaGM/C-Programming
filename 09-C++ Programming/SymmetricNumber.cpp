ea3ccc1001b3799a527d7a0ac9b05378b009af22
Ghulam Mustafa <mustafaji@gmail.com>
2021-02-04 23:08:34 +0100

    C++ Programming - Symmetric number

---
+++ 09-C++ Programming/SymmetricNumber.cpp
@@ -0,0 +1,20 @@
+// Symmetric number
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1000; i < 10000; i++)
+    {
+        if (((i / 1000) == (i % 10)) && (((i / 100) % 10) == ((i / 10) % 10)))
+        {
+            cout << i << endl;
+        }
+        else
+        {
+            cout << "";
+        }
+    }
+    return 0;
+}
