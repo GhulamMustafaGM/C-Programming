e6a077ca3a2c8bd01f52680b951b978227bd7ebb
Ghulam Mustafa <mustafaji@gmail.com>
2021-02-08 21:46:07 +0100

    C++ Programming - Find digital number

---
+++ 09-C++ Programming/FindDigitalNumber.cpp
@@ -0,0 +1,27 @@
+// Digit sub number finder
+
+#include <iostream>
+#include <string>
+using namespace std;
+main()
+{
+    int range, digit, d = 10;
+    cin >> range >> digit;
+    for (int i = 0, number = 0; i <= range; i++, number = i)
+    {
+        if (i == digit)
+            cout << i << " ";
+        else
+            while (number)
+            {
+                if (number >= d * 10)
+                    d *= 10;
+                if (number % d == digit || number % (d / 10) == digit)
+                {
+                    cout << i << "  ";
+                    break;
+                }
+                number /= 10;
+            }
+    }
+}
