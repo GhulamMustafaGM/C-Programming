cf8b4fdadbc6126c6c25c1aa4c826591069b01ee
Ghulam Mustafa <mustafaji@gmail.com>
2021-02-13 22:18:22 +0100

    C++ Programming - Dynamic memory

---
+++ 10 - C++ Programming/DynamicMemory.cpp
@@ -0,0 +1,16 @@
+// dynamic memory
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int *p = new int; // request memory
+    *p = 5;           // store value
+
+    cout << *p << endl; // use value
+
+    delete p; // free up the memory
+
+    return 0;
+}
