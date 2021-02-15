3beb3d8c92b057024b0fe9048b8c64def6cf502f
Ghulam Mustafa <mustafaji@gmail.com>
2021-02-15 22:19:38 +0100

    C++ Programming - SizeOf

---
+++ 11-C++ Programming/SizeOf.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "int size = " << sizeof(int) << ",";
+    std::cout << "short isze = " << sizeof(short) << ",";
+    std::cout << "long isze = " << sizeof(long) << ",";
+    std::cout << "char size = " << sizeof(char) << ", ";
+    std::cout << "float size = " << sizeof(float) << ", ";
+    std::cout << "double size = " << sizeof(double) << ",";
+    std::cout << "bool size = " << sizeof(bool) << ",";
+
+    return 0;
+}
